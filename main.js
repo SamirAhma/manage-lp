26ee7fd2cf6269c96ac8003f2981c5f771eb063f
SamirAhma <30921483+SamirAhma@users.noreply.github.com>
2023-03-13 18:55:36 +0800

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import "./style.css";
+
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
